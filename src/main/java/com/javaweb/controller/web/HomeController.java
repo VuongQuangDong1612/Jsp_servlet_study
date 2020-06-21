d12a5e9c7772f171261cd1de3943caa4b3f19430
ADMIN <quangdong1612@gmail.com>
2020-06-21 09:03:44 +0700

    commit

--- src/main/java/com/javaweb/controller/web/HomeController.java
+++ src/main/java/com/javaweb/controller/web/HomeController.java
@@ -8,11 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/trang-chu"})
+@WebServlet(urlPatterns = {"/trangchu"})
 public class HomeController extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher rd = req.getRequestDispatcher("/views/web/Home.jsp");
+        RequestDispatcher rd = req.getRequestDispatcher("/views/web/home.jsp");
         rd.forward(req, resp);
     }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,16 +1,10 @@
 <%--
   Created by IntelliJ IDEA.
   User: ADMIN
-  Date: 6/20/2020
-  Time: 10:10 PM
+  Date: 6/21/2020
+  Time: 8:56 AM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>aaaaa</title>
-</head>
-<body>
-    <h1>zzzzzzzzzzzzzzzz</h1>
-</body>
-</html>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+<c:redirect url="/trangchu"/>
